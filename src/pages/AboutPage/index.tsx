e35d38936cd72f8ec26524838908db061d077aae
shubhamkunwar10 <fonders@metakul.com>
2025-04-06 21:25:26 +0530

    Add pages for layout

---
+++ src/pages/AboutPage/index.tsx
@@ -0,0 +1,14 @@
+function Homepage() {
+
+    return (
+      <>
+        <h1>About Us</h1>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </>
+    )
+  }
+  
+  export default Homepage
+  
